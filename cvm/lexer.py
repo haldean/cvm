8d1533c9b21ee9fef3e8afd31562258db21988b3
Will Brown <will.h.brown@gmail.com>
2012-01-04 15:24:17 -0800

    All operators now recognized by lexer.
    Nothing has happened in the parser though, so it complains quite loudly about
    unused tokens. Test code added to lexer.py, such that when you run:
    
      python cvm/lexer.py (5 + 7) * 6
    
    It will show the result of tokenization of the provided arguments.

--- cvm/lexer.py
+++ cvm/lexer.py
@@ -32,6 +32,8 @@ def mklex(reserved, tokens):
     print('Illegal character "%s"' % t.value[0])
     t.lexer.skip(1)
 
+  # Constants
+
   def t_floatpt_constant(t):
     r'(?P<value>([0-9]+\.[0-9]*|[0-9]*\.[0-9]+)([eE][+-]?[0-9]+)?)(?P<flags>[fFlL]?)'
     t.value = (float(t.lexer.lexmatch.group('value')),
@@ -40,22 +42,22 @@ def mklex(reserved, tokens):
     return t
 
   def t_floatpt_constant_nodecimal(t):
-    r'(?P<value>([1-9][0-9]*|0))(?P<flags>[fFlLdD]?)'
+    r'(?P<value>([1-9][0-9]*|0))(?P<flags>[fFlLdD])'
     t.value = (float(t.lexer.lexmatch.group('value')),
         parse_floatpt_flags(t.lexer.lexmatch.group('flags')))
+    t.type = 'NUMBER'
+    return t
+
+  def t_decimal_int_constant(t):
+    r'(?P<value>[1-9][0-9]*)(?P<flags>[uUlL]{0,2})'
+    t.value = (int(t.lexer.lexmatch.group('value')),
+        parse_integral_flags(t.lexer.lexmatch.group('flags')))
     t.type = 'NUMBER'
     return t
 
   def t_hex_constant(t):
     r'(?P<value>0[xX][a-fA-F0-9]+)(?P<flags>[uUlL]{0,2})'
     t.value = (int(t.lexer.lexmatch.group('value'), 16),
-        parse_integral_flags(t.lexer.lexmatch.group('flags')))
-    t.type = 'NUMBER'
-    return t
-
-  def t_decimal_int_constant(t):
-    r'(?P<value>[1-9][0-9]+)(?P<flags>[uUlL]{0,2})'
-    t.value = (int(t.lexer.lexmatch.group('value')),
         parse_integral_flags(t.lexer.lexmatch.group('flags')))
     t.type = 'NUMBER'
     return t
@@ -67,6 +69,67 @@ def mklex(reserved, tokens):
     t.type = 'NUMBER'
     return t
 
+  # Operators and other misc. punctuation
+
+  t_ADD = r'\+'
+  t_ADD_ASSIGN = r'\+\='
+  t_AND_ASSIGN = r'&='
+  t_ASSIGN = r'='
+  t_BITWISE_AND = r'&'
+  t_BITWISE_NOT = r'~'
+  t_BITWISE_OR = r'\|'
+  t_BITWISE_XOR = r'\^'
+  t_BOOLEAN_AND = r'&&'
+  t_BOOLEAN_NOT = r'\!'
+  t_BOOLEAN_OR = r'\|\|'
+  t_CLOSE_BRACE = r'\}'
+  t_COLON = r':'
+  t_COMMA = r','
+  t_DECREMENT = r'\-\-'
+  t_DEREF_ARROW = r'\-\>'
+  t_DIVIDE = r'/'
+  t_DIVIDE_ASSIGN = r'/='
+  t_ELLIPSIS = r'\.\.\.'
+  t_EQ = r'=='
+  t_GEQ = r'\>='
+  t_INCREMENT = r'\+\+'
+  t_LBRACE = r'\['
+  t_LBRACKET = r'\<'
+  t_LEFT_ASSIGN = r'\<\<='
+  t_LEQ = r'\<='
+  t_LPAREN = r'\('
+  t_LSHIFT = r'\<\<'
+  t_MINUS = r'-'
+  t_MODULUS = r'%'
+  t_MODULUS_ASSIGN = r'%='
+  t_MULTIPLY_ASSIGN = r'\*\='
+  t_NEQ = r'!='
+  t_OPEN_BRACE = r'\{'
+  t_OR_ASSIGN = r'\|='
+  t_PERIOD = r'\.'
+  t_RBRACE = r'\]'
+  t_RBRACKET = r'\>'
+  t_RIGHT_ASSIGN = r'\>\>='
+  t_RPAREN = r'\)'
+  t_RSHIFT = r'\>\>'
+  t_SEMICOLON = r';'
+  t_STAR = r'\*'
+  t_SUBTRACT_ASSIGN = r'-='
+  t_TERNARY = r'\?'
+  t_XOR_ASSIGN = r'\^='
+
   import ply.lex as lex
   return lex.lex()
 
+def test_lex():
+  import env
+  import cvm.parse
+  import sys
+
+  lx = mklex(cvm.parse.reserved, cvm.parse.tokens)
+  lx.input(' '.join(sys.argv[1:]))
+  for tok in lx:
+    print(tok)
+
+if __name__ == '__main__':
+  test_lex()
